c88c1d2b5bd2341e44008d3ed5b72548f90752d1
ryuryuryuk <kellyryu04@gmail.com>
2026-02-19 02:14:56 +0900

    fresh commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['http://localhost:3000', "https://heaveless-naomi-incontestably.ngrok-free.dev" ],                         
 };
 
 export default nextConfig;
